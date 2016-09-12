6a427008b07d6e6aa9c590faa082498331ae4e52
Hunter Douglas Hostelley <hostelleyhd@dcsdk12.org>
2016-09-12 12:28:47 -0600

    Fixed version

--- quiz.py
+++ quiz.py
@@ -1,41 +1,41 @@
 	
-def has_teen(teen):
-	if teen <= 19 and teen >=13:
+def has_teen(a,b,c):
+	if a <= 19 and a >=13 or b <= 19 and b >=13 or c <= 19 and c >=13:
 		return True
 	else:
 		return False
 
-print has_teen(13) # expect True
-print has_teen(14) # expect True
-print has_teen(5) # expect False
-print has_teen(58) # expect False
+print has_teen(19, 1, 3) # expect True
+print has_teen(5, 15, 4) # expect True
+print has_teen(9, 5, 18) # expect False
+print has_teen(58, 60, 85) # expect False
 
 def not_string(note):
 	if note.startswith ("not"):
 		return note + ("not")
 	else: 
-		return ("not ") + note
+		return ("not") + note
 
 print not_string("friendly") # expect not friendly
 print not_string("not a good ") # expect not a good not
 
-def icy_hot(icy, hot):
-	if icy < 0 and hot > 100:
+def icy_hot(temp, temp2):
+	if (temp < 0 and temp2 > 100) or (temp > 100 and temp2 < 0) :
 		return True
 	else:
 		return False
 
 print icy_hot(-1, 125) # expect True
 print icy_hot(52, 48) # expect False
-print icy_hot(52, 524) # expect False
+print icy_hot(-4, 101) # expect True
 print icy_hot(-5, 24) # expect False
 
 def closer_to(a,b,n):
-	if a < n and b > n:
-		return "a"
-	if b < n and a > n:
-		return "b"
-	if a < n or b > n:
+	if abs(a - b) < abs(a - n):
+		return b
+	if abs(a - b) > abs(a - n):
+		return n
+	if abs(a - b) == abs(a - n):
 		return 0
 
 print closer_to(3, 5, 6) # expect b
